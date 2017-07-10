e4c2096258472b59c869e32219f3ec3d96b584ec
David RJ <david.rud135@gmail.com>
2017-07-10 11:29:11 +0300

    Some fixes in code

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function reverse() {
 	const userInput = document.getElementById('input').value;
-	const stringLen = userInput.length;
 	const stringArr = userInput.split('');
 	const reversedStrArr = stringArr.reverse();
 	const result = reversedStrArr.join('');
-	var resultDiv = document.getElementById('result');
+	const resultDiv = document.getElementById('result');
 	resultDiv.style.display = 'block';
 	return resultDiv.innerHTML = result;  	
 }
