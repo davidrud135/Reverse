8153d826ee5cb3f74f92e97cba41edd369957935
David RJ <david.rud135@gmail.com>
2017-08-30 21:24:26 +0300

    jQuery events and new ready functions

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function reverse() {
-	const userInput = document.getElementById('input').value;
-	const stringArr = userInput.split('');
-	const reversedStrArr = stringArr.reverse();
-	const result = reversedStrArr.join('');
-	const resultDiv = document.getElementById('result');
-	resultDiv.style.display = 'block';
-	return resultDiv.innerHTML = result;  	
-}
+$(document).ready(function() {
+
+	$('form').submit(function reverseString() {
+		var userString, arrayOfStrings, reversedArrOfStrs, resultString;
+
+		userString = getInputData();
+		arrayOfStrings = createArrayOfStrings(userString);
+		reversedArrOfStrs = reverseArray(arrayOfStrings);
+		resultString = makeStringFromArray(reversedArrOfStrs);
+
+		showResult(resultString);
+		return false;
+	});
+
+	function getInputData() {
+		return $('input[type=text]').val();
+	}
+
+	function showResult(result) {
+		$('#result').html(result);
+	}
+
+	function createArrayOfStrings(userString) {
+		return userString.split('');
+	}
+
+	function reverseArray(array) {
+		return array.reverse();
+	}
+
+	function makeStringFromArray(array) {
+		return array.join('');
+	}
+});
